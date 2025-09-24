15e4c08d02dd4639c159624783fc7eb99ca806d5
techtitansofficial2025 <techtitans.official2025@gmail.com>
2025-09-25 04:11:26 +0530

    Update __init__.py

--- predictor/__init__.py
+++ predictor/__init__.py
@@ -1,1 +1,5 @@
+# top of backend/main.py — MUST come before importing tensorflow
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = ""   # force CPU-only; prevents TF from trying to init CUDA
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # optional: reduce TF logs
 default_app_config = 'predictor.apps.PredictorConfig'
