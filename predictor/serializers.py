6e921ccbe5a12a9fb85d26444009100893292d20
techtitansofficial2025 <techtitans.official2025@gmail.com>
2025-09-24 23:59:44 +0530

    Create serializers.py

---
+++ predictor/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+class PredictRequestSerializer(serializers.Serializer):
+    red = serializers.FloatField()
+    ir = serializers.FloatField()
+    gender = serializers.ChoiceField(choices=['male','female','Male','Female','M','F'])
+    age = serializers.FloatField(min_value=0, max_value=130)
+
+    def validate_gender(self, value):
+        v = str(value).lower()
+        if v in ['m','male']:
+            return 'male'
+        if v in ['f','female']:
+            return 'female'
+        raise serializers.ValidationError("Gender must be male/female")
+
+class PredictResponseSerializer(serializers.Serializer):
+    predicted_hb = serializers.FloatField()
