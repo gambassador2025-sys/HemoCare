769e1f7a78dab9a8b32f7c4484cf002524610330
techtitansofficial2025 <techtitans.official2025@gmail.com>
2025-09-25 08:50:53 +0530

    Update asgi.py

--- hbppg_backend/asgi.py
+++ hbppg_backend/asgi.py
@@ -1,5 +1,7 @@
 import os
-from django.core.asgi import get_asgi_application
+os.environ.setdefault("CUDA_VISIBLE_DEVICES", "-1")
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
 
+from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hbppg_backend.settings')
 application = get_asgi_application()
